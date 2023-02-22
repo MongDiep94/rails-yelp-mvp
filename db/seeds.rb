5065e8146c9cd5e0ab2f97c33c631878b387c1a8
mong diep <le.mongdiep@gmail.com>
2023-02-22 19:35:51 +0100

    Yelp MVC finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+5.times do
+  category = ["chinese", "italian", "japanese", "french", "belgian"]
+  Restaurant.create(name: Faker::Book.title,
+                    address: Faker::Address.full_address,
+                    category: category[rand(0..5)])
+end
